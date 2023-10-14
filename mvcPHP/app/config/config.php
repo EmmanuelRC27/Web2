701c361152b4a795188348eab51352547e086d18
EmmanuelRC27 <135392714+EmmanuelRC27@users.noreply.github.com>
2023-10-14 16:04:01 -0600

    Add files via upload

---
+++ mvcPHP/app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+			//database params in constant
+			define('db_Servidor' , '127.0.0.1');
+			define('db_Basedato' , 'prueba');
+			define('db_Usuario'  , 'root');
+			define('db_Contra'   , 'Utn123**');
+			
+			//absolut address
+			define('appRoot',dirname(dirname(__FILE__)));
+
+			//relative addres
+			define('urlRoot','http://10.90.30.80/MVCPHP');
+
+			//relative address
+			define('siteName','Ejemplo de MVC con PHP');
+		?>
---
+++ mvcPHP/public/index.php
@@ -0,0 +1,5 @@
+<?php
+            // echo 'Página principal'; <!-- eliminar esta línea -->
+			require_once('../app/require.php');
+        ?>
+	
